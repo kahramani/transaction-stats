00efafda13449f7deca9d9bec63a15faceaab719
Ibrahim Kahraman <polapierre@gmail.com>
2018-07-15 16:47:32 +0300

    adds maven surefire plugin for functional tests

--- src/test/java/com/kahramani/txstats/controller/StatisticsControllerFT.java
+++ src/test/java/com/kahramani/txstats/controller/StatisticsControllerFT.java
@@ -1,13 +1,12 @@
 package com.kahramani.txstats.controller;
 
-import com.kahramani.txstats.model.request.TransactionRequest;
+import com.kahramani.txstats.model.response.StatsResponse;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,21 +14,18 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.time.Instant;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- * Integration tests for /transactions endpoint.
+ * Functional tests for /statistics endpoint
  * Checks if everything works right in spring application context and relations between business layers
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
-public class TransactionControllerIT {
+public class StatisticsControllerFT {
 
-    private final static String ENDPOINT = "/api/v1/transactions";
-    private final static String REFRESH_ENDPOINT = "/api/v1/refresh";
+    private final static String ENDPOINT = "/api/v1/statistics";
 
     @Autowired
     private TestRestTemplate testRestTemplate;
@@ -38,20 +34,14 @@ public class TransactionControllerIT {
     private int serverPort;
 
     @Test
-    public void should_return_http_201() {
-        TransactionRequest transactionRequest = new TransactionRequest();
-        transactionRequest.setAmount(0.0d);
-        transactionRequest.setTimestamp(Instant.now().toEpochMilli());
-
+    public void should_get_statistic_with_null_body() {
         // call the endpoint
         UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl("http://localhost:" + serverPort + ENDPOINT);
-        ResponseEntity<Void> response = testRestTemplate.exchange(builder.build().encode().toUri(), HttpMethod.POST, new HttpEntity<>(transactionRequest), Void.class);
+        ResponseEntity<StatsResponse> response = testRestTemplate.exchange(builder.build().encode().toUri(), HttpMethod.GET, null, StatsResponse.class);
 
         // assertions
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
-
-        // this method dirties context so refresh needed
-        UriComponentsBuilder refreshUri = UriComponentsBuilder.fromHttpUrl("http://localhost:" + serverPort + REFRESH_ENDPOINT);
-        testRestTemplate.exchange(refreshUri.build().encode().toUri(), HttpMethod.GET, null, Void.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        StatsResponse statsResponse = response.getBody();
+        assertThat(statsResponse).isNotNull();
     }
 }
--- src/test/java/com/kahramani/txstats/controller/TransactionControllerFT.java
+++ src/test/java/com/kahramani/txstats/controller/TransactionControllerFT.java
@@ -1,12 +1,13 @@
 package com.kahramani.txstats.controller;
 
-import com.kahramani.txstats.model.response.StatsResponse;
+import com.kahramani.txstats.model.request.TransactionRequest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,18 +15,21 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.time.Instant;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- * Integration tests for /statistics endpoint
+ * Functional tests for /transactions endpoint.
  * Checks if everything works right in spring application context and relations between business layers
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
-public class StatisticsControllerIT {
+public class TransactionControllerFT {
 
-    private final static String ENDPOINT = "/api/v1/statistics";
+    private final static String ENDPOINT = "/api/v1/transactions";
+    private final static String REFRESH_ENDPOINT = "/api/v1/refresh";
 
     @Autowired
     private TestRestTemplate testRestTemplate;
@@ -34,14 +38,20 @@ public class StatisticsControllerIT {
     private int serverPort;
 
     @Test
-    public void should_get_statistic_with_null_body() {
+    public void should_return_http_201() {
+        TransactionRequest transactionRequest = new TransactionRequest();
+        transactionRequest.setAmount(0.0d);
+        transactionRequest.setTimestamp(Instant.now().toEpochMilli());
+
         // call the endpoint
         UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl("http://localhost:" + serverPort + ENDPOINT);
-        ResponseEntity<StatsResponse> response = testRestTemplate.exchange(builder.build().encode().toUri(), HttpMethod.GET, null, StatsResponse.class);
+        ResponseEntity<Void> response = testRestTemplate.exchange(builder.build().encode().toUri(), HttpMethod.POST, new HttpEntity<>(transactionRequest), Void.class);
 
         // assertions
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        StatsResponse statsResponse = response.getBody();
-        assertThat(statsResponse).isNotNull();
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+
+        // this method dirties context so refresh needed
+        UriComponentsBuilder refreshUri = UriComponentsBuilder.fromHttpUrl("http://localhost:" + serverPort + REFRESH_ENDPOINT);
+        testRestTemplate.exchange(refreshUri.build().encode().toUri(), HttpMethod.GET, null, Void.class);
     }
 }
